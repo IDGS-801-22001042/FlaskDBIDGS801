cd109a16220fe126a7b5a8d67dcf93036d091ae9
IDGS-801-22001042 <80253@alumnos.utleon.edu.mx>
2025-02-26 20:18:21 -0600

    26-02-2025 FlaskDBIDGS801

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from sqlalchemy import create_engine
+
+import urllib
+
+class Config(object):
+    SECRET_KEY='Clave nueva'
+    SESSION_COOKIE_SECRET=False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Asakalaluya1$@127.0.0.1/bdidgs801'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
